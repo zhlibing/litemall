3d17c1789322ba859ffba4318c0e0f1edf716111
zhlibing <42637117@qq.com>
2019-07-28 12:51:56 +0800

    详情页评论条数20条

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxGoodsController.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxGoodsController.java
@@ -120,7 +120,7 @@ public class WxGoodsController {
 
 		// 评论
 		Callable<Map> commentsCallable = () -> {
-			List<LitemallComment> comments = commentService.queryGoodsByGid(id, 0, 2);
+			List<LitemallComment> comments = commentService.queryGoodsByGid(id, 0, 20);
 			List<Map<String, Object>> commentsVo = new ArrayList<>(comments.size());
 			long commentCount = PageInfo.of(comments).getTotal();
 			for (LitemallComment comment : comments) {
