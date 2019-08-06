8d5ad36f388d5a99e462bcb0e196a39feff13f4c
lbzhang3 <42637117@qq.com>
2019-08-06 10:27:53 +0800

    居中+

--- litemall-vue/src/components/Tabbar/index.vue
+++ litemall-vue/src/components/Tabbar/index.vue
@@ -1,88 +1,92 @@
 <template>
-	<van-tabbar v-model="active" style="z-index: 1999">
-		<van-tabbar-item
-			v-for="(tab, index) in tabbar"
-			:icon="tab.icon"
-			:to="tab.path"
-			:dot="tab.dot"
-			:info="tab.info"
-			:key="index">
-			{{tab.name}}
-		</van-tabbar-item>
-	</van-tabbar>
+    <van-tabbar v-model="active" style="z-index: 1999">
+        <van-tabbar-item
+                v-for="(tab, index) in tabbar"
+                :icon="tab.icon"
+                :to="tab.path"
+                :dot="tab.dot"
+                :info="tab.info"
+                :key="index">
+            {{tab.name}}
+        </van-tabbar-item>
+    </van-tabbar>
 </template>
 
 
 <script>
-import { Tabbar, TabbarItem } from 'vant';
+    import {Tabbar, TabbarItem} from 'vant';
 
-export default {
-  data() {
-    return {
-      active: 0,
-      tabbar: [
-        {
-          name: '精选',
-          path: '/',
-          pathName: 'home',
-          icon: 'compass-full',
-          dot: false,
-          info: ''
+    export default {
+        data() {
+            return {
+                active: 0,
+                tabbar: [
+                    {
+                        name: '精选',
+                        path: '/',
+                        pathName: 'home',
+                        icon: 'compass-full',
+                        dot: false,
+                        info: ''
+                    },
+                    {
+                        name: '鱼塘',
+                        path: '/yutang',
+                        pathName: 'yutang',
+                        icon: 'class-full',
+                        dot: false,
+                        info: ''
+                    },
+                    {
+                        dot: false,
+                        info: ''
+                    },
+                    {
+                        name: '荣誉榜',
+                        path: '/rankings',
+                        pathName: 'rankings',
+                        icon: 'cart-full',
+                        dot: false,
+                        info: ''
+                    },
+                    {
+                        name: '我的',
+                        path: '/user',
+                        pathName: 'user',
+                        icon: 'wode',
+                        dot: false,
+                        info: ''
+                    }
+                ]
+            };
         },
-        {
-          name: '鱼塘',
-          path: '/yutang',
-          pathName: 'yutang',
-          icon: 'class-full',
-          dot: false,
-          info: ''
+
+        watch: {
+            $route: 'changeActive'
         },
-        {
-          name: '荣誉榜',
-          path: '/rankings',
-          pathName: 'rankings',
-          icon: 'cart-full',
-          dot: false,
-          info: ''
+
+        created() {
+            const toName = this.$route.name;
+            this.setActive(toName);
         },
-        {
-          name: '我的',
-          path: '/user',
-          pathName: 'user',
-          icon: 'wode',
-          dot: false,
-          info: ''
+
+        methods: {
+            changeActive({name}) {
+                this.setActive(name);
+            },
+            setActive(name) {
+                this.tabbar.forEach((tab, i) => {
+                    if (tab.pathName == name) {
+                        this.active = i;
+                        return false;
+                    }
+                });
+            }
+        },
+
+        components: {
+            [Tabbar.name]: Tabbar,
+            [TabbarItem.name]: TabbarItem
         }
-      ]
     };
-  },
-
-  watch: {
-    $route: 'changeActive'
-  },
-
-  created() {
-    const toName = this.$route.name;
-    this.setActive(toName);
-  },
-
-  methods: {
-    changeActive({ name }) {
-      this.setActive(name);
-    },
-    setActive(name) {
-      this.tabbar.forEach((tab, i) => {
-        if (tab.pathName == name) {
-          this.active = i;
-          return false;
-        }
-      });
-    }
-  },
-
-  components: {
-    [Tabbar.name]: Tabbar,
-    [TabbarItem.name]: TabbarItem
-  }
-};
 </script>
--- litemall-vue/src/components/head/floatbutton.vue
+++ litemall-vue/src/components/head/floatbutton.vue
@@ -3,6 +3,7 @@
         <div class="outbox">
             <span class="open" @click=""></span>
         </div>
+        <span class="van-tabbar-item">发布</span>
     </div>
 </template>
 <script>
@@ -10,24 +11,28 @@
 </script>
 <style lang="scss" scoped>
     .float_b {
-        z-index: 200;
-        height: 2.75rem;
-        width: 2.75rem;
+        z-index: 2999;
+        height: 3rem;
+        width: 3rem;
         position: fixed;
-        bottom: 60px;
-        right: 20px;
+        bottom: 20px;
+        center: true;
         display: flex;
         align-items: center;
+        margin-left: 43%;
+        pointer-events: auto;
+        flex-direction: column;
         .outbox {
-            width: 2.75rem;
-            height: 2.75rem;
+            width: 3rem;
+            height: 3rem;
             border-radius: 50%;
-            margin: 0.15rem auto;
+            margin: 0.18rem auto;
+            background-color: #ffffff;
             display: block;
             z-index: 500;
             .open {
-                width: 2.75rem;
-                height: 2.75rem;
+                width: 2.5rem;
+                height: 2.5rem;
                 display: block;
                 border-radius: 50%;
                 margin: 0.1rem auto;
