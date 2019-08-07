e360a05a6430e8460dd9bea2280a0ce58f1fed74
lbzhang3 <42637117@qq.com>
2019-08-07 15:09:18 +0800

    发布心情

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxCircleController.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxCircleController.java
@@ -5,17 +5,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.linlinjava.litemall.core.system.SystemConfig;
 import org.linlinjava.litemall.core.util.ResponseUtil;
-import org.linlinjava.litemall.db.domain.LitemallComment;
-import org.linlinjava.litemall.db.domain.LitemallFootprint;
-import org.linlinjava.litemall.db.domain.LitemallCircle;
-import org.linlinjava.litemall.db.domain.LitemallUser;
+import org.linlinjava.litemall.db.domain.*;
 import org.linlinjava.litemall.db.service.*;
 import org.linlinjava.litemall.wx.annotation.LoginUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
@@ -141,4 +136,36 @@ public class WxCircleController {
         return ResponseUtil.ok(goodsCount);
     }
 
+    @PostMapping("save")
+    public Object save(@LoginUser Integer userId, @RequestBody LitemallCircle circle) {
+        if (userId == null) {
+            return ResponseUtil.unlogin();
+        }
+
+        if (circle.getId() == null || circle.getId().equals(0)) {
+            circle.setId(null);
+            circle.setUserId(userId);
+            CircleService.add(circle);
+        } else {
+            circle.setUserId(userId);
+            if (CircleService.updateById(circle) == 0) {
+                return ResponseUtil.updatedDataFailed();
+            }
+        }
+        return ResponseUtil.ok(circle.getId());
+    }
+
+    @PostMapping("delete")
+    public Object delete(@LoginUser Integer userId, @RequestBody LitemallCircle circle) {
+        if (userId == null) {
+            return ResponseUtil.unlogin();
+        }
+        Integer id = circle.getId();
+        if (id == null) {
+            return ResponseUtil.badArgument();
+        }
+
+        CircleService.deleteById(id);
+        return ResponseUtil.ok();
+    }
 }
